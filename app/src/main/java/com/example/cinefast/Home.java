355c83f578e057d2dbd34e88970dac005426f35e
rustamali9900 <144158447+rustamali9900@users.noreply.github.com>
2026-02-15 22:27:10 +0500

    styling movie screen

--- app/src/main/java/com/example/cinefast/Home.java
+++ app/src/main/java/com/example/cinefast/Home.java
@@ -1,6 +1,10 @@
 package com.example.cinefast;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,39 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Home extends AppCompatActivity {
 
+    Button btnGetStarted;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        getWindow().setStatusBarColor(getResources().getColor(R.color.black));
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        init();
+
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                {
+                    Intent i = new Intent(Home.this, Movies.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        });
     }
+
+    private void init()
+    {
+        btnGetStarted = findViewById(R.id.btnGetStarted);
+    }
+
+
 }
