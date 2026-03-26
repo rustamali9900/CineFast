0894b6494e39338b76d92bb0ab57713b35dc1a99
rustamali9900 <144158447+rustamali9900@users.noreply.github.com>
2026-03-26 06:08:21 +0500

    initialising movie class

---
+++ app/src/main/java/com/example/cinefast/Movie.java
@@ -0,0 +1,20 @@
+package com.example.cinefast;
+
+public class Movie {
+    private String title;
+    private String details;
+    private int imageResId; 
+    private String trailerQuery;
+
+    public Movie(String title, String details, int imageResId, String trailerQuery) {
+        this.title = title;
+        this.details = details;
+        this.imageResId = imageResId;
+        this.trailerQuery = trailerQuery;
+    }
+
+    public String getTitle() { return title; }
+    public String getDetails() { return details; }
+    public int getImageResId() { return imageResId; }
+    public String getTrailerQuery() { return trailerQuery; }
+}
