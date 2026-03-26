2fa4ac3d0f0a9efcb042a3946dea2bd2cbe83ecd
rustamali9900 <144158447+rustamali9900@users.noreply.github.com>
2026-03-26 05:16:51 +0500

    adding splash act

--- app/src/main/java/com/example/cinefast/Splash.java
+++ app/src/main/java/com/example/cinefast/Splash.java
@@ -13,23 +13,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Splash extends AppCompatActivity {
 
+    ImageView logo;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.splash), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        init();
+        playAnimation();
+        moveToHome();
     }
 
+    private void init()
+    {
+        logo = findViewById(R.id.logo);
+    }
 
-
+    private void playAnimation()
+    {
+        Animation slideUp = AnimationUtils.loadAnimation(this, R.anim.slide_up);
+        logo.startAnimation(slideUp);
+    }
+    private void moveToHome()
+    {
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(Splash.this, Home.class);
+            startActivity(intent);
+            finish();
+        }, 1000);
+    }
 }
