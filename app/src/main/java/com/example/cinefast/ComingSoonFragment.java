af0d0ea64d271c8b6beff83d70462b396e356cf2
rustamali9900 <144158447+rustamali9900@users.noreply.github.com>
2026-03-26 07:17:18 +0500

    comingSoon fragment and importing images for movieData

--- app/src/main/java/com/example/cinefast/ComingSoonFragment.java
+++ app/src/main/java/com/example/cinefast/ComingSoonFragment.java
@@ -1,60 +1,46 @@
 package com.example.cinefast;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ComingSoonFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ComingSoonFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ComingSoonFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ComingSoonFragment newInstance(String param1, String param2) {
-        ComingSoonFragment fragment = new ComingSoonFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_coming_soon, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView_comingSoon);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        List<Movie> upcomingMovies = new ArrayList<>();
+        upcomingMovies.add(new Movie("The Shawshank Redemption", "Drama | 2h 22m", R.drawable.shawshank, "The Shawshank Redemption trailer"));
+        upcomingMovies.add(new Movie("Ironman", "Action | 1h 49m", R.drawable.ironman, "Ironman trailer"));
+        upcomingMovies.add(new Movie("Dune: Part Three", "Sci-Fi | 1h 57m", R.drawable.dune, "Dune Messiah trailer"));
+
+        MovieAdapter adapter = new MovieAdapter(upcomingMovies, new MovieAdapter.OnMovieClickListener() {
+            @Override
+            public void onBookSeatsClick(Movie movie) {
+                Toast.makeText(getContext(), "Selected: " + movie.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
     }
 }
